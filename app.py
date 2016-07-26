003e31645eed4442e717bb7a194037c70e7c2ef0
Jens Hinkelmann <JH@Jenss-MacBook-Pro.local>
2016-07-26 14:01:07 +0200

    added get an post for products, moved views to view folder

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.httpauth import HTTPBasicAuth
 import json
@@ -13,15 +13,26 @@ db = SQLAlchemy(app)
 
 from models import *
 
+@app.route('/products', methods=['GET'])
+def get_products():
 
-@app.route('/')
-def hello():
-    import time
-    visit=Visits("/")
+    #TODO: log visits automatically
+    visit = Visits("/products")
     db.session.add(visit)
     db.session.commit()
-    time.sleep(5)
-    return json.dumps(([str(a.__repr__()) for a in Visits.query.all()]), indent=4)
+
+    return json.dumps(([str(a.__repr__()) for a in Product.query.all()]), indent = 4)
+
+@app.route('/products', methods=['POST'])
+def post_product():
+    product = Product()
+    product.name = request.json['name']
+    product.specification = request.json['specification']
+    product.englishName = request.json['englishName']
+    product.frenchName = request.json['frenchName']
+    db.session.add(product)
+    db.session.commit()
+
 
 @app.route('/<name>')
 @auth.login_required
--- models.py
+++ models.py
@@ -1,6 +1,11 @@
-import datetime
+import datetime, json, jsonpickle
 from app import db
+from sqlalchemy import Table, Column, Integer, ForeignKey
 from sqlalchemy.dialects.postgresql import JSON
+from sqlalchemy.orm import relationship
+from sqlalchemy.ext.declarative import declarative_base
+
+Base=declarative_base()
 
 
 class Visits(db.Model):
@@ -17,70 +22,83 @@ class Visits(db.Model):
     def __repr__(self):
         return 'id {}'.format(self.id)
 
-class Category(db.Model):
+# class Category(db.Model):
     #Product prototype
+
+class Tag_Product_association(db.Model):
+    __tablename__ = 'tag_prod_association'
+    product_id = db.Column(db.Integer, db.ForeignKey('product.id'), primary_key=True)
+    tag_name= db.Column(db.String, db.ForeignKey('tag.name'), primary_key=True)
+
+class Co2_Product_association(db.Model):
+    __tablename__ = 'co2_prod_association'
+    product_id = db.Column(db.Integer, db.ForeignKey('product.id'), primary_key=True)
+    co2_id= db.Column(db.Integer, db.ForeignKey('co2.id'), primary_key=True)
 
 class Product(db.Model):
     __tablename__='product'
 
-    id = db.Column(db.integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String())
     specification = db.Column(db.String())
-    synonyms=db.Column(db.sqlalchemy.types.ARRAY(db.String()))
+    # synonyms=db.Column(db.sqlalchemy.types.ARRAY(db.String()))
     englishName=db.Column(db.String())
     frenchName=db.Column(db.String())
+    tags = relationship("Tag", secondary="tag_prod_association")
 
-    #Advanced
-    tags
-    alternatives
-    standardOrigin
-    possibleOrigins
-    productionMethods
-    productionMethodParameters
-    degreesOfProcessing
-    degreesOfProcessingParameters
-    preservationMethods
-    packaging
-    packagingParameters
-    packagingMethods
-    startOfLocalSeason
-    endOfLocalSeason
-    density
-    unitWeight
-    commentsOnDensityAndUnitWeight
-    referencesOndensityAndUnitWeight
-    Texture
-    Foodwaste
-    CommentsOnFoodwaste
-    Allergenes
+    def __repr__(self):
+        return jsonpickle.encode(self.__dict__)
+    # #Advanced
+    # alternatives
+    # standardOrigin
+    # possibleOrigins
+    # productionMethods
+    # productionMethodParameters
+    # degreesOfProcessing
+    # degreesOfProcessingParameters
+    # preservationMethods
+    # packaging
+    # packagingParameters
+    # packagingMethods
+    # startOfLocalSeason
+    # endOfLocalSeason
+    # density
+    # unitWeight
+    # commentsOnDensityAndUnitWeight
+    # referencesOndensityAndUnitWeight
+    # Texture
+    # Foodwaste
+    # CommentsOnFoodwaste
+    # Allergenes
 
-    #Documentation
-    CO2CalculationPath
-    CalculationProcessDocumentation
-    InfoTextForCook
-    ReferencesForBasicCO2Value
-    OtherReferences
-    CommentsOnFoodwasteCO2CalculationPathForDifferentProductParameters
-    DataQualityEstimation
+    # #Documentation
+    # CO2CalculationPath
+    # CalculationProcessDocumentation
+    # InfoTextForCook
+    # ReferencesForBasicCO2Value
+    # OtherReferences
+    # CommentsOnFoodwasteCO2CalculationPathForDifferentProductParameters
+    # DataQualityEstimation
 
-class FoodWasteCollection(db.Model)
-    __tablename__ = 'foodwaste'
-    product=db.Column(db.ForeignKey)
-    productionAvoidable = db.Column(db.integer())
+# class FoodWasteCollection(db.Model):
+#     __tablename__ = 'foodwaste'
+#     product=db.Column(db.ForeignKey)
+#     productionAvoidable = db.Column(db.integer())
 
-class Value(db.Model)
-    __tablename__ = 'value'
+class Tag(db.Model):
+    __tablename__='tag'
 
-class ValueBase(Co2Value)
-    __tablename__ = 'base'
+    name=db.Column(db.String, primary_key=True)
 
-class ValueDerived(Co2Value)
-    __tablename__ = 'derived'
+class Co2Value(db.Model):
+    __tablename__='co2'
+    id=db.Column(db.Integer, primary_key=True)
+    value=db.Column(db.String)
 
-class Co2Value(Value)
+# class CO2ValueDerived(Co2Value):
 
-class Reference(db.Model)
-    __tablename__ = 'reference'
+# class CO2ValueBase(Co2Value):
 
-#TODO: many to many Reference <-> Value
+# class Reference(db.Model)
+#     __tablename__ = 'reference'
 

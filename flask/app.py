5c6bd324aff589c3c69441d2dacb8f6d28af8e87
Jens Hinkelmann <JH@Jenss-MacBook-Pro.local>
2016-08-02 00:31:31 +0200

    added alternatives to product get api

--- flask/app.py
+++ flask/app.py
@@ -5,8 +5,7 @@ from flask.ext.httpauth import HTTPBasicAuth
 import json
 auth = HTTPBasicAuth()
 import os
-
-
+"""Provides an API for models, intended for use with javascript and electron"""
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -21,8 +20,7 @@ def get_products():
     visit = Visits("/products")
     db.session.add(visit)
     db.session.commit()
-    intermediate = Product.query.all()
-    return jsonify([a.toDict() for a in intermediate])
+    return jsonify([a.toDict() for a in EdbProduct.query.all()])
 
 @app.route('/product/<id>', methods=['GET'])
 def get_product(id):
@@ -30,19 +28,64 @@ def get_product(id):
     visit = Visits("/product")
     db.session.add(visit)
     db.session.commit()
-    intermediate = EdbProduct.query.get(id)
+    intermediate = Product.query.get(id)
     return jsonify(intermediate.toDict())
 
-@app.route('/products', methods=['POST'])
-def post_product():
-    product = Product()
-    product.name = request.json['name']
-    product.specification = request.json['specification']
-    product.englishName = request.json['englishName']
-    product.frenchName = request.json['frenchName']
+@app.route('/product', methods=['PUT'])
+def put_product():
+    if 'edb' in request.json:
+        if request.json['edb']:
+            product = EdbProduct()
+        else:
+            product=TemplateProduct()
+    else:
+        product=TemplateProduct()
+    if not 'name' in request.json:
+        abort
+    else:
+        product.name = request.json['name']
+    if 'specification' in request.json:
+        product.specification = request.json['specification']
+    if 'englishName' in request.json:
+        product.englishName = request.json['englishName']
+    if 'frenchName' in request.json:
+        product.frenchName = request.json['frenchName']
+    if 'alternatives' in request.json:
+        for alternativeId in request.json['alternatives']:
+            try:
+                alternative=Product.query.get(alternativeId)
+                product.alternatives.append(alternative)
+            except:
+                pass
+    if 'synonyms' in request.json:
+        for synonym in request.json['synonyms']:
+            pass
+    if 'tags' in request.json:
+        for tag in request.json['tags']:
+            pass
+    if 'nutrients' in request.json:
+        for nutrient in request.json['nutrients']:
+            pass
+    if 'processes' in request.json:
+        for process in request.json['processes']:
+            pass
+    if 'possibleOrigins' in request.json:
+        for origin in request.json['possibleOrigins']:
+            pass
+    if 'allergenes' in request.json:
+        for allergene in request.json['allergenes']:
+            pass
+    if 'co2Value' in request.json:
+        pass
+    if 'standardOrigin' in request.json:
+        pass
+    if 'density' in request.json:
+        pass
+    if 'unitWeight' in request.json:
+        pass
     db.session.add(product)
     db.session.commit()
-
+    return "ok"
 
 
 @app.route('/<name>')
--- flask/models.py
+++ flask/models.py
@@ -30,18 +30,24 @@ class Value(db.Model):
     }
 
     id=db.Column(db.Integer, primary_key=True)
-    
     validCountries = relationship("Location", secondary="location_scivalue_association")
     reference_id=db.Column(db.Integer, db.ForeignKey('reference.id'))
+    reference=relationship("Reference")
     base_value_id=db.Column(db.Integer, db.ForeignKey('scivalue.id'))
     baseValue=relationship("Value", foreign_keys=base_value_id)
+    amount=db.Column(db.Integer)
+    def actualValue(self):
+        if base_value_id:
+            return baseValue.actualValue()
+        else:
+            return self
 
-#Countries for standard/possible origin attribute
+#Countries
 class Location(db.Model):
     __tablename__='location'
     id=db.Column(db.Integer, primary_key=True)
     name=db.Column(db.String)
-    possibleProducts=relationship("Product", secondary="location_prod_association", back_populates="possibleLocations")
+    possibleProducts=relationship("Product", secondary="location_prod_association", back_populates="possibleOrigins")
 
 #A single product's single foodwaste number
 class FoodWasteData(Value):
@@ -57,7 +63,8 @@ class FoodWasteData(Value):
 class Process(db.Model):
     __tablename__='process'
     id=db.Column(db.Integer, primary_key=True)
-    name=db.Column(db.String)
+    name=db.Column(db.String())
+    description=db.Column(db.String())
 
 #An allergene not tied to a product
 class Allergene(db.Model):#single allergene, not multiple allergenes
@@ -78,6 +85,11 @@ class Tag(db.Model):
     __tablename__='tag'
     name=db.Column(db.String, primary_key=True)
 
+#A Synonym, to be connected with products
+class Synonym(db.Model):
+    __tablename__ = 'synonym'
+    name=db.Column(db.String, primary_key=True)
+
 #A single product's CO2Value
 class Co2Value(Value):
     __tablename__='co2'
@@ -87,7 +99,7 @@ class Co2Value(Value):
     id = Column(db.Integer, ForeignKey('scivalue.id'), primary_key=True)
     scivalue=db.Column(db.String)
     product_id = Column(Integer, ForeignKey('product.id'))
-    product = relationship("Product", back_populates="Co2Value")
+    product = relationship("Product", back_populates="co2Value")
 
 #A reference, maybe for multiple values
 class Reference(db.Model):
@@ -101,6 +113,12 @@ class TagProductAssociation(db.Model):
     product_id = db.Column(db.Integer, db.ForeignKey('product.id'), primary_key=True)
     tag_name= db.Column(db.String, db.ForeignKey('tag.name'), primary_key=True)
 
+#Associate products with relevant tags for searching
+class SynonymProductAssociation(db.Model):
+    __tablename__ = 'synonym_prod_association'
+    product_id = db.Column(db.Integer, db.ForeignKey('product.id'), primary_key=True)
+    synonym_name= db.Column(db.String, db.ForeignKey('synonym.name'), primary_key=True)
+
 #Associate one allergene to a product
 class ProductAllergeneAssociation(Value):
     __tablename__ = 'prod_allergene_association'
@@ -111,6 +129,7 @@ class ProductAllergeneAssociation(Value):
     product_id = db.Column(db.Integer, db.ForeignKey('product.id'), primary_key=False)
     allergene_id= db.Column(db.Integer, db.ForeignKey('allergene.id'), primary_key=False)
     product=relationship("Product", back_populates="allergenes")
+    allergene=relationship("Allergene")
 
 #Store how much one process changes one nutrient for one product
 class ProductProcessNutritionAssociation(Value):
@@ -141,11 +160,11 @@ class ProductNutrientAssociation(Value):
     __mapper_args__ = {
         'polymorphic_identity':'prod_nutrient_association',
     }
-
     id = Column(db.Integer, ForeignKey('scivalue.id'), primary_key=True)
     product_id = db.Column(db.Integer, db.ForeignKey('product.id'))
-    nutrient_id= db.Column(db.Integer, db.ForeignKey('nutrient.id'))
     product=relationship("Product", back_populates="nutrients")
+    nutrient_id= db.Column(db.Integer, db.ForeignKey('nutrient.id'))
+    nutrient=relationship("Nutrient")
 
 class ProductDensity(Value):
     __tablename__ = 'density'
@@ -179,7 +198,7 @@ class LocationProductAssociation(db.Model):
     product_id = db.Column(db.Integer, db.ForeignKey('product.id'), primary_key=True)
     location_id= db.Column(db.Integer, db.ForeignKey('location.id'), primary_key=True)
 
-#Associate onee value to the locations in which it is valid
+#Associate one value to the locations in which it is valid
 class LocationValueAssociation(db.Model): #valid for
     __tablename__ = 'location_scivalue_association'
     location_id = db.Column(db.Integer, db.ForeignKey('location.id'), primary_key=True)
@@ -193,25 +212,84 @@ class Product(db.Model):
         'polymorphic_identity':'product',
         'polymorphic_on':type
     }
+
+    # def __init__(self, allergenes, alternatives, englishName, frenchName,
+    #     name, nutrients, processes, possibleOrigins, specification,
+    #     synonyms, tags, co2Value, standardOrigin, startOfLocalSeason,
+    #     endOfLocalSeason, density, unitWeight, commentsOnDensityAndUnitWeight,
+    #     texture, foodWasteData, infoTextForCook):
+    #     self.name=name
+    #     self.allergenes=allergenes
+    #     self.alternatives=alternatives
+    #     self.englishName=englishName
+    #     self.frenchName=frenchName
+    #     self.name=name
+    #     self.nutrients=nutrients
+    #     self.processes=processes
+    #     self.possibleOrigins=possibleOrigins
+    #     self.specification=specification
+    #     self.synonyms=synonyms
+    #     self.tags=tags
+    #     self.co2Value=co2Value
+    #     self.standardOrigin=standardOrigin
+    #     self.startOfLocalSeason=startOfLocalSeason
+    #     self.endOfLocalSeason=endOfLocalSeason
+    #     self.density=density
+    #     self.unitWeight=unitWeight
+    #     self.commentsOnDensityAndUnitWeight=commentsOnDensityAndUnitWeight
+    #     self.texture=texture
+    #     self.foodWasteData=foodWasteData
+    #     self.infoTextForCook=infoTextForCook
+        
+    def fromJson(jsonObject):
+        product.name = request.json['name']
+        product.specification = request.json['specification']
+        product.englishName = request.json['englishName']
+        product.frenchName = request.json['frenchName']
+        product.frenchName = request.json['frenchName']
+
     def toDict(self):
-        return {'id': self.id, 'name': self.name, 'specification': self.specification}
+        response={'id': self.id, 'name': self.name,
+        'specification': self.specification,
+        'englishName': self.englishName,
+        'frenchName':self.frenchName,
+        'alternatives':[{'name':product.name, 'id':product.id} for product in self.alternatives],
+        'synonyms':[synonym.name for synonym in self.synonyms],
+        'tags':[tag.name for tag in self.tags],
+        'nutrients':[{'amount':nutrient.amount,'name':nutrient.nutrient.name} for nutrient in self.nutrients],
+        'processes':[{'id':process.process.id, 'name':process.process.name} for process in self.processes],
+        'possibleOrigins':[origin.name for origin in self.possibleOrigins],
+        'allergenes':[allergene.allergene.name for allergene in self.allergenes]
+        }
+        if self.co2Value:
+            response['hasCo2Value']=(self.co2Value.actualValue().id==self.co2Value.id())
+            response['co2Value']=self.co2Value.actualValue().amount
+        if self.standardOrigin:
+            response['standardOrigin']=self.standardOrigin.name
+        if self.density:
+            response['density']=self.density.amount
+        if self.unitWeight:
+            response['unitWeight']=self.unitWeight.amount
+        return response
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String())
-    specification = db.Column(db.String())
-    # synonyms=relationship("Synonym", secondary="synonym_prod_association")
+
+    allergenes=relationship("ProductAllergeneAssociation", back_populates="product")
+    alternatives=relationship("Product", secondary="prod_alternatives", primaryjoin=id==ProductAlternative.product_id_1,
+                       secondaryjoin=id==ProductAlternative.product_id_2)
     englishName=db.Column(db.String())
     frenchName=db.Column(db.String())
-    Co2Value=relationship("Co2Value", uselist=False, back_populates="product")
-    tags = relationship("Tag", secondary="tag_prod_association")
+    name = db.Column(db.String())
     nutrients=relationship("ProductNutrientAssociation", back_populates="product")
-    allergenes=relationship("ProductAllergeneAssociation", back_populates="product")
-    #Advanced
-    alternatives=relationship("Product", secondary="prod_alternatives", primaryjoin=id==ProductAlternative.product_id_1,
-                           secondaryjoin=id==ProductAlternative.product_id_2)
+    processes=relationship("ProductProcessNutritionAssociation", back_populates="product")
+    possibleOrigins=relationship("Location", secondary="location_prod_association", back_populates="possibleProducts")
+    specification = db.Column(db.String())
+    synonyms=relationship("Synonym", secondary="synonym_prod_association")
+    tags = relationship("Tag", secondary="tag_prod_association")
+
+    co2Value=relationship("Co2Value", uselist=False, back_populates="product")
     standardOrigin_id = db.Column(db.Integer, db.ForeignKey('location.id'))
     standardOrigin=relationship(Location, foreign_keys=standardOrigin_id)
-    possibleLocations=relationship("Location", secondary="location_prod_association", back_populates="possibleProducts")
     ##replace multiple fields with processes list
     # productionMethods
     # productionMethodParameters
@@ -221,11 +299,11 @@ class Product(db.Model):
     # packaging
     # packagingParameters
     # packagingMethods
-    processes=relationship("ProductProcessNutritionAssociation", back_populates="product")
+    
     startOfLocalSeason=db.Column(db.Date())
     endOfLocalSeason=db.Column(db.Date())
     density=relationship("ProductDensity", back_populates="product")
-    unitWeight=relationship("unitWeight", back_populates="product")
+    unitWeight=relationship("ProductUnitWeight", back_populates="product")
     commentsOnDensityAndUnitWeight=db.Column(db.String())
     texture=db.Column(db.String)
     foodWasteData=relationship("FoodWasteData", back_populates="product")
@@ -248,7 +326,7 @@ class EdbProduct(Product):
     }
     id = Column(db.Integer, ForeignKey('product.id'), primary_key=True)
 
-#A food product  prototype, can be seen as category
+#A food product prototype, can be seen as category
 class TemplateProduct(Product):
     __tablename__ = 'template'
     __mapper_args__ = {

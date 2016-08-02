7f68469ace3c375b7d80f1ffb775f5d09fc99020
Jens Hinkelmann <schinke@users.noreply.github.com>
2016-08-02 12:00:39 +0200

    refactor editProduct into controllor.py

--- flask/app.py
+++ flask/app.py
@@ -3,6 +3,7 @@ from flask import jsonify, Flask, request, Response
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.httpauth import HTTPBasicAuth
 import json
+import controller
 auth = HTTPBasicAuth()
 import os
 """Provides an API for models, intended for use with javascript and electron"""
@@ -47,7 +48,8 @@ def put_product():
         product.name = request.json['name']
     db.session.add(product)
     db.session.commit()
-    return post_product(product.id)
+    id=controller.editProduct(product.id,request)
+    return str(id)
 
 @app.route('/product/<id>', methods=['POST'])
 def post_product(id):
@@ -57,104 +59,8 @@ def post_product(id):
         product = None
     if not product:
         return "resource not found"#TODO: add appropriate status code
-    if 'name' in request.json:
-        product.name = request.json['name']
-    if 'specification' in request.json:
-        product.specification = request.json['specification']
-    if 'englishName' in request.json:
-        product.englishName = request.json['englishName']
-    if 'frenchName' in request.json:
-        product.frenchName = request.json['frenchName']
-    if 'alternatives' in request.json:
-        for alternativeId in request.json['alternatives']:
-            try:
-                alternative=Product.query.get(alternativeId)
-                product.alternatives.append(alternative)
-            except:
-                pass
-    if 'synonyms' in request.json:
-        for synonymName in request.json['synonyms']:
-            try:
-                synonym=Synonym.query.get(synonymName)
-            except:
-                synonym=None
-            if not synonym:
-                synonym=Synonym(synonymName)
-                #side effect
-                db.session.add(synonym)
-            product.synonyms.append(synonym)
-    if 'tags' in request.json:
-        for tagName in request.json['tags']:
-            try:
-                tag=Tag.query.get(tagName)
-            except:
-                tag=None
-            if not tag:
-                tag=Tag()
-                tag.name=tagName
-                #side effect
-                db.session.add(tag)
-            product.tags.append(tag)
-    if 'nutrients' in request.json:
-        for nutrientDict in request.json['nutrients']:
-            if 'name' in nutrientDict and 'amount' in nutrientDict:
-                try:
-                    nutrient=Nutrient.query.filter(name==nutrientDict['name'])
-                except:
-                    nutrient=None
-                if not nutrient:
-                    nutrient=Nutrient()
-                    nutrient.name=nutrientDict['name']
-                    #side effect
-                    db.session.add(nutrient)
-                association=ProductNutrientAssociation()
-                db.session.add(association)
-                product.nutrients.append(association)
-                association.nutrient=nutrient
-                association.amount=nutrientDict['amount']
-    if 'processes' in request.json:
-        for processDict in request.json['processes']:
-            if 'name' in processDict and 'nutrient' in processDict\
-            and 'amount' in processDict:
-                try:
-                    process=Process.query.filter(name==processDict['name'])
-                except:
-                    process=None
-                if not process:
-                    process=Process()
-                    process.name=processDict['name']
-                    db.session.add(process)
-                try:
-                    nutrient=Nutrient.query.filter(name==processDict['nutrient'])
-                except:
-                    nutrient=None
-                if not nutrient:
-                    nutrient=Nutrient()
-                    nutrient.name=processDict['nutrient']
-                    db.session.add(nutrient)
-                association=ProductProcessNutritionAssociation()
-                association.process=process
-                association.product=product
-                association.nutrient=nutrient
-                association.amount=processDict['amount']
-                db.session.add(association)
-            #TODO: check if dict
-    if 'possibleOrigins' in request.json:
-        for origin in request.json['possibleOrigins']:
-            pass
-    if 'allergenes' in request.json:
-        for allergene in request.json['allergenes']:
-            pass
-    if 'co2Value' in request.json:
-        pass
-    if 'standardOrigin' in request.json:
-        pass
-    if 'density' in request.json:
-        pass
-    if 'unitWeight' in request.json:
-        pass
-    db.session.add(product)
-    db.session.commit()
+    else:
+        controller.editProduct(id,request)
     return "ok"
 
 
